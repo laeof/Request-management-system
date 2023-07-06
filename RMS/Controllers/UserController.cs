e056fac996b15cdcc7c6e6c33683efcbba753852
laeof <turututuuuuuuu@gmail.com>
2023-07-06 16:48:47 +0300

    little front-end change and bug fix
    fixed manager can activate/disactivate delete edit admin

--- RMS/Controllers/UserController.cs
+++ RMS/Controllers/UserController.cs
@@ -34,34 +34,76 @@ namespace RMS.Controllers
         public IActionResult Edit(uint id)
         {
             var userrole = dataManager.UserRole.GetUserRoleById(id);
+
+            if (userrole == null)
+                return RedirectToAction("Users");
+
+            var user_role = userrole.RoleId;
+
+            var current_user_role = dataManager.UserRole.GetUserRole()
+            .FirstOrDefault(role => role.UserId == Convert.ToUInt32(Request.Cookies["Id"]))
+            .RoleId;
+
+            //if current user is manager or less
+            if (current_user_role >= 2)
+                if (current_user_role > user_role)
+                    return RedirectToAction("Users");
+
             userrole.User = dataManager.Users.GetUserById(userrole.UserId);
             userrole.User.Id = userrole.UserId;
             userrole.Role = dataManager.Role.GetRoleById(userrole.RoleId);
             userrole.UserRoleId = id;
+
             return View(userrole);
         }
 		[Authorize(Roles = "admin, manager")]
 		[HttpPost]
         public IActionResult Edit(UserRole userrole)
         {
-            if (userrole != null)
-            {
-                dataManager.Users.SaveUser(userrole.User);
-                dataManager.UserRole.SaveUserRole(userrole);
-            }
+
+            if (userrole == null)
+                return RedirectToAction("Users");
+
+            var user_role = userrole.RoleId;
+
+            var current_user_role = dataManager.UserRole.GetUserRole()
+            .FirstOrDefault(role => role.UserId == Convert.ToUInt32(Request.Cookies["Id"]))
+            .RoleId;
+
+            //if current user is manager or less
+            if (current_user_role >= 2)
+                if (current_user_role > user_role)
+                    return RedirectToAction("Users");
+
+            dataManager.Users.SaveUser(userrole.User);
+            dataManager.UserRole.SaveUserRole(userrole);
 
             return RedirectToAction("Users");
         }
 		[Authorize(Roles = "admin, manager")]
 		public IActionResult ActivityChange(uint id)
         {
-            var user = dataManager.Users.GetUserById(id);
+            var user = dataManager.Users.GetUserById(id); 
+            
+            if(user == null)
+                return RedirectToAction("Users");
 
-            if (user != null)
-            {
-                user.IsActive = !user.IsActive;
-                dataManager.Users.SaveUser(user);
-            }
+            var user_role = dataManager.UserRole.GetUserRole()
+            .FirstOrDefault(role => role.UserId == id)
+            .RoleId;
+
+            var current_user_role = dataManager.UserRole.GetUserRole()
+            .FirstOrDefault(role => role.UserId == Convert.ToUInt32(Request.Cookies["Id"]))
+            .RoleId;
+
+            //if current user is manager or less
+            if(current_user_role >= 2)
+                if (current_user_role > user_role)
+                    return RedirectToAction("Users");
+
+            user.IsActive = !user.IsActive;
+            dataManager.Users.SaveUser(user);
+
             return RedirectToAction("Users");
         }
 		[Authorize(Roles = "admin, manager")]
@@ -69,10 +111,23 @@ namespace RMS.Controllers
         {
             var user = dataManager.Users.GetUserById(id);
 
-            if (user != null)
-            {
-                dataManager.Users.DeleteUser(user);
-            }
+            if (user == null)
+                return RedirectToAction("Users");
+
+            var user_role = dataManager.UserRole.GetUserRole()
+            .FirstOrDefault(role => role.UserId == id)
+            .RoleId;
+
+            var current_user_role = dataManager.UserRole.GetUserRole()
+            .FirstOrDefault(role => role.UserId == Convert.ToUInt32(Request.Cookies["Id"]))
+            .RoleId;
+
+            //if current user is manager or less
+            if (current_user_role >= 2)
+                if (current_user_role > user_role)
+                    return RedirectToAction("Users");
+
+            dataManager.Users.DeleteUser(user);
 
             return RedirectToAction("Users");
         }
